e589c94eecab7f749e3551eb304f915aa4dabda4
@daaronr <8229168+daaronr@users.noreply.github.com>
2023-06-05 20:10:55 -0400

    code to input evaluation data from airtable, output it for people on team to access

---
+++ code/shared_packages_code.R
@@ -0,0 +1,67 @@
+
+library(pacman)
+p_load(dplyr, knitr, ggplot2, here, bookdown, janitor, DT,  install = FALSE)
+p_load(readxl, readr, pryr, stringr, kableExtra, install = FALSE)
+p_load(magrittr, install = FALSE)
+p_load(devtools, googledrive,  googlesheets4, install = FALSE)
+#p_load(BayesianFirstAid, install = FALSE)
+#powerjoins
+
+#devtools::install_github("paulhendricks/anonymizer")
+#library(anonymizer)
+
+  
+#devtools::install_github("rethinkpriorities/rp-r-package")
+library(rethinkpriorities)
+
+#devtools::install_github("rethinkpriorities/r-noodling-package")
+library(rnoodling)
+
+#Erin used:
+p_load(data.table,
+  tidyr,
+  digest,
+  tidyverse,
+  stringr,
+  lmtest,
+  plm,
+  sandwich,
+  miceadds,
+  quantreg,
+  readxl,
+  psych,
+  dplyr,
+  plotrix,
+  qualtRics,
+  psych,
+  pastecs,
+  dplyr,
+  tidyverse,
+  ggplot2,
+  tidyr,
+  broom,
+  corrr,
+  nFactors,
+  GPArotation,
+  reshape2,
+  skimr,
+  sjmisc,
+  modeest,
+  DescTools,
+  plm,
+  lmtest,
+  effectsize,
+  ggeffects,
+  mediation,
+  miceadds,
+  lavaan,
+  jtools,
+  scales,
+  RColorBrewer,
+  plotrix,
+  scales
+)
+
+
+
+
