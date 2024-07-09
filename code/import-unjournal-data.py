418b55b482abd0387c3259e591995bf76123254c
David Hugh-Jones <davidhughjones@gmail.com>
2024-07-09 11:32:00 +0100

    Ignore columns with whitespace

--- code/import-unjournal-data.py
+++ code/import-unjournal-data.py
@@ -7,6 +7,13 @@ from codaio import Coda, Document
 from dotenv import load_dotenv
 import os
 import pandas as pd
+import re
+
+def strip_columns_with_space(df):
+  cols = df.columns
+  cols_with_space = [col for col in cols if re.search(" ", col) is not None]
+  df = df.drop(columns = cols_with_space)
+  return df
 
 # We share API variables with .Renviron, which uses a similar format
 # Note that on github the .Renviron path *should not* be there. Instead,
@@ -18,9 +25,16 @@ coda_api_key = os.environ["CODA_API_KEY"]
 
 coda = Coda(coda_api_key)
 
-# This is the file of evaluations
 doc = Document("0KBG3dSZCs", coda = coda)
-research = doc.get_table("grid-Iru9Fra3tE")
 
+# This is the file of evaluations
+research = doc.get_table("grid-Iru9Fra3tE")
 research = pd.DataFrame(research.to_dict())
-research.to_csv("data/evals.csv")
+research = strip_columns_with_space(research)
+research.to_csv("data/research.csv")
+
+# Evaluator ratings
+rsx_evalr_rating = doc.get_table("grid-pcJr9ZM3wT")
+rsx_evalr_rating = pd.DataFrame(rsx_evalr_rating.to_dict())
+rsx_evalr_rating = strip_columns_with_space(rsx_evalr_rating)
+rsx_evalr_rating.to_csv("data/rsx_evalr_rating.csv")
